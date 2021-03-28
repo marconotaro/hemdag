a61538dd90e146c88240a4e845799380f8fff901
marconotaro <notaromarco90@gmail.com>
2021-03-28 15:56:08 +0200

    update hemdag checker

--- docs/playground/script/hemdag-checker.R
+++ docs/playground/script/hemdag-checker.R
@@ -1,54 +1,56 @@
 #!/usr/bin/Rscript
 
-start.time <- proc.time();
-
-library(HEMDAG);
+suppressPackageStartupMessages(library(HEMDAG));
 library(optparse);
 
 optionList <- list(
-    make_option(c("-e", "--exptype"), type="character", default="ho",
-        help="type of dataset on which run HEMDAG. It can be: ho (hold-out) or cv (cross-validated) -- def. ho")
+    make_option(c("-o", "--organism"), type="character", default="7227_drome",
+        help="organism name in the form <taxon>_<name> (def 7227_drome)"),
+    make_option(c("-d", "--domain"),  type="character", default="mf",
+        help="gene ontology domain -- bp, mf, cc (def. mf)"),
+     make_option(c("-e", "--exptype"), type="character", default="ho",
+        help="type of dataset on which run HEMDAG. It can be: ho (hold-out) or cv (cross-validated) -- def. ho"),
+    make_option(c("-f", "--flat"), type="character", default="svmlinear",
+        help="flat classifier (def. svmlinear)"),
+    make_option(c("-a", "--algorithm"), type="character", default="isodescensTAU",
+        help="hierarchical correction algorithm (def. isodescensTAU)")
 );
 
 optParser <- OptionParser(option_list=optionList);
 opt <- parse_args(optParser);
 
+## setting input argument
+prefix    <- opt$organism;
+taxon     <- strsplit(prefix,"_")[[1]][1];
+organism  <- strsplit(prefix,"_")[[1]][2];
+domain    <- opt$domain;
 exptype   <- opt$exptype;
+flat      <- opt$flat;
+algorithm <- opt$algorithm;
 
+## I/O
 data.dir <- paste0("../data/", exptype, "/");
 res.dir  <- paste0("../res/",  exptype, "/");
 data.files <- list.files(data.dir);
 res.files  <- list.files(res.dir);
+if(!dir.exists(res.dir)){dir.create(res.dir, recursive=TRUE);}
 
-orgs  <- c("7227_drome"); ## organism(s) list
-flats <- c("svmlinear");  ## flat classifier(s) list
-algs  <- c("gpav", "isotprTF", "isotprW", "isodescensTF", "isodescensW", "isodescensTAU");  ## HEMDAG algorithm(s) list
-onts  <- c("mf");   ## GO domain(s) list (bp, mf, cc)
+## load data
+dag.file  <- data.files[grep(paste0(domain,".dag"), data.files)];
+hier.file <- res.files[grep(paste0(organism, ".*", domain, ".scores.*", flat, ".", algorithm), res.files)];
 
-for(org in orgs){
-    for(flat in flats){
-        for(alg in algs){
-            for(ont in onts){
-                dag.file  <- data.files[grep(paste0(ont,".dag"), data.files)];
-                hier.file <- res.files[grep(paste0(org, ".*", ont, ".scores.*", flat, ".", alg), res.files)];
-                g <- get(load(paste0(data.dir, dag.file)));
-                S.hier <- get(load(paste0(res.dir, hier.file)));
-                root <- root.node(g);
-                g <- build.subgraph(colnames(S.hier), g);
-                check <- check.hierarchy(S.hier, g, root);
-                if(check$status=="OK"){
-                    cat(org, ont, paste0(flat,'+',alg), "check passed :)","\n");
-                }else{
-                    cat(org, ont, paste0(flat,'+',alg), "check failed :(","\n");
-                }
-            }
-        }
-    }
+## check if flat|ann|dag exists
+if(length(dag.file)==0 || length(hier.file)==0)
+    stop("no dag|hier file found\n");
+
+## check constraints violation
+g <- get(load(paste0(data.dir, dag.file)));
+S.hier <- get(load(paste0(res.dir, hier.file)));
+root <- root.node(g);
+g <- build.subgraph(colnames(S.hier), g);
+check <- check.hierarchy(S.hier, g, root);
+if(check$status=="OK"){
+    cat(taxon, organism, domain, paste0(flat,'+',algorithm), "check passed :)","\n");
+}else{
+    cat(taxon, organism, domain, paste0(flat,'+',algorithm), "check failed :(","\n");
 }
-
-timing.s <- proc.time() - start.time;
-timing.m <- round(timing.s/(60),4);
-timing.h <- round(timing.m/(60),4);
-cat("\n");
-cat("elapsed time:", timing.s["elapsed"], "(seconds)", "|", timing.m["elapsed"], "(minutes)", "|" , timing.h["elapsed"], "(hours)", "\n\n");
-
